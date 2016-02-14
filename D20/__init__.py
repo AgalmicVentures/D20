3c7d03affa9e9462d4315886572471d9f7c129f7
Ian Hutchinson <ihutchinson@agalmicventures.com>
2016-02-14 10:56:15 -0600

    Moved config to server.conf

--- D20/__init__.py
+++ D20/__init__.py
@@ -8,13 +8,8 @@ import Application
 def main():
 	print('Starting up D20...')
 
-	cherrypy.config.update({
-		'server.socket_host': '0.0.0.0',
-		'server.socket_port': 27184,
-	})
-
 	d20 = Application.D20Application()
-	cherrypy.quickstart(d20)
+	cherrypy.quickstart(d20, config='server.conf')
 
 	print('Shutting down D20...')
 	return 0
