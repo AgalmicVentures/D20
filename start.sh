9a4a9ad32f852313fd338c98579c778acfd2f7ea
Ian Hutchinson <ihutchinson@agalmicventures.com>
2016-04-24 18:39:13 -0500

    Added safety flags to shell scripts

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+set -u
+
 PROCESSES=`ps xa | grep D20/__init__.py | grep -v grep`
 if [[ $? -eq 0 ]]; then
 	echo "Already running"
 	exit
 fi
 
+set -e
+
 echo "Starting..."
 nohup ./D20/__init__.py &
 sleep 1
 echo "Started."
-
