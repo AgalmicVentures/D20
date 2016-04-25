976755878b5e8c5b56c58e88bbf3fb9f806d6f28
Ian Hutchinson <ihutchinson@agalmicventures.com>
2016-04-24 20:48:19 -0500

    Cleaner service architecture

--- start.sh
+++ start.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 set -u
+source service.sh
 
-CWD=`pwd`
-PROCESSES=`ps xa | grep $CWD/D20/__init__.py | grep -v grep`
-if [[ $? -eq 0 ]]; then
-	echo "Already running"
-	exit
-fi
-
-set -e
-
-echo "Starting..."
-nohup $CWD/D20/__init__.py &
-sleep 1
-echo "Started."
+start
