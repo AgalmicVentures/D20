cddb6bebc7ee7b5a2b83a6549b22d84a26776f8b
Ian Hutchinson <ihutchinson@agalmicventures.com>
2017-06-11 17:43:55 -0500

    Refactor roll script to use functions

--- roll.sh
+++ roll.sh
@@ -25,18 +25,46 @@ set -u
 readonly START_WAIT_MAX=15
 readonly STEP_WAIT_MAX=2
 
+#Waits for a random period of time from 0 to some number of seconds
+randomWait() {
+	python3 -c "import random, time; time.sleep($1 * random.random())"
+}
+
+#Queries an individual entropy server
+queryServer() {
+	readonly SERVER="$1"
+	echo "Getting entropy from $SERVER ..."
+
+	#Calculate the hash of the highest resolution time available as a challenge (%N is nanos for GNU date)a
+	readonly START_TIME=$(date +%Y%m%d%H%M%S%N)
+	readonly CHALLENGE=$(echo "$START_TIME" | $SHA512 | cut -d' ' -f1)
+	readonly EXPECTED_CHALLENGE_RESPONSE=$(echo -n "$CHALLENGE" | $SHA512 | cut -d' ' -f1)
+
+	#Query the server and check the challenge response
+	readonly RESPONSE=$(curl -s "$SERVER/api/entropy?challenge=$CHALLENGE")
+	readonly CHALLENGE_RESPONSE=$(echo "$RESPONSE" | grep -Eo '"challengeResponse": "[0-9a-f]+' | cut -d'"' -f4)
+	if [ "$CHALLENGE_RESPONSE" = "$EXPECTED_CHALLENGE_RESPONSE" ]
+	then
+		#Pass through sha512sum to complicate writing a malicious server
+		echo "$RESPONSE" | $SHA512 > /dev/urandom
+		echo "Success."
+	else
+		echo "Invalid challenge response (got $CHALLENGE_RESPONSE, expected $EXPECTED_CHALLENGE_RESPONSE)"
+	fi
+}
+
 #Get the server pool
 if [[ $# -gt 0 ]]
 then
 	echo "Seeding local entropy pool from $# servers"
-	SERVERS="$*"
+	readonly SERVERS="$*"
 else
 	echo "Seeding local entropy pool from default server"
-	SERVERS=https://agalmicventures.com:8443
+	readonly SERVERS=https://agalmicventures.com:8443
 fi
 
 echo "Waiting to start (randomized to prevent server contention)..."
-python3 -c "import random, time; time.sleep($START_WAIT_MAX * random.random())"
+randomWait "$START_WAIT_MAX"
 
 #Alias sha512sum if necessary on OS X
 if which sha512sum ; then
@@ -49,27 +77,8 @@ fi
 #Get the entropy from each server
 for SERVER in $SERVERS
 do
-	echo "Getting entropy from $SERVER ..."
-
-	#Calculate the hash of the highest resolution time available as a challenge (%N is nanos for GNU date)a
-	START_TIME=$(date +%Y%m%d%H%M%S%N)
-	CHALLENGE=$(echo "$START_TIME" | $SHA512 | cut -d' ' -f1)
-	EXPECTED_CHALLENGE_RESPONSE=$(echo -n "$CHALLENGE" | $SHA512 | cut -d' ' -f1)
-
-	#Query the server and check the challenge response
-	RESPONSE=$(curl -s "$SERVER/api/entropy?challenge=$CHALLENGE")
-	CHALLENGE_RESPONSE=$(echo "$RESPONSE" | grep -Eo '"challengeResponse": "[0-9a-f]+' | cut -d'"' -f4)
-	if [ "$CHALLENGE_RESPONSE" = "$EXPECTED_CHALLENGE_RESPONSE" ]
-	then
-		#Pass through sha512sum to complicate writing a malicious server
-		echo "$RESPONSE" | $SHA512 > /dev/urandom
-		echo "Success."
-	else
-		echo "Invalid challenge response (got $CHALLENGE_RESPONSE, expected $EXPECTED_CHALLENGE_RESPONSE)"
-	fi
-
-	#Random wait in between
-	python3 -c "import random, time; time.sleep($STEP_WAIT_MAX * random.random())"
+	queryServer "$SERVER"
+	randomWait "$STEP_WAIT_MAX"
 done
 
 echo "Finished seeding local entropy pool."
